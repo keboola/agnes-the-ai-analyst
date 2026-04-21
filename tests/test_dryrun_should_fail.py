f2791eadd5ad3deea010b9e7eff9fd607ba6bc64
ZdenekSrotyr <zdenek.srotyr@keboola.com>
2026-04-21 21:32:41 +0200

    Revert "dryrun: intentional failing test (will be reverted)"
    This reverts commit cf9cc06a7884bb401ff29fc5cb6d8baf84dc3daa.

--- tests/test_dryrun_should_fail.py
+++
@@ -1,3 +0,0 @@
-def test_intentional_fail_for_dryrun():
-    """Intentional failure to verify CI gate blocks broken PRs. Remove after dryrun."""
-    assert False, "dryrun: this test is supposed to fail"
